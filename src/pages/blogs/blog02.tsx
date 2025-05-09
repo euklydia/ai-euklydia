bd87e23920cfe5175b5038fb9efb00e2bbb03b56
LeonOZ <zahhari.otmane@gmail.com>
2025-05-09 11:03:41 +0200

    push new version

--- src/pages/blogs/blog02.tsx
+++ src/pages/blogs/blog02.tsx
@@ -1,11 +1,5 @@
 import { Footer } from "../../components/footer/Footer";
 import { Header } from "../../components";
-import IMAGE01 from "../../assets/images/blog/Photo-1.jpg";
-import IMAGE02 from "../../assets/images/blog/Baya-et-nesrine.jpg";
-import IMAGE03 from "../../assets/images/blog/Photo-3.jpg";
-import IMAGE04 from "../../assets/images/blog/Photo-4.png";
-import ICON1 from "../../assets/images/blog/icone-1.png";
-import ICON2 from "../../assets/images/blog/icone-2.png";
 import "./styles.css";
 
 export const Blog01 = () => {
@@ -29,7 +23,7 @@ export const Blog01 = () => {
                 </p>
               </div>
               <div className="blog-image">
-                <img src={IMAGE01} alt="" />
+                <img src={""} alt="" />
                 <div className="bg-image-blog"></div>
               </div>
             </div>
